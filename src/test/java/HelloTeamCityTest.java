7e170093dc5648c4fe9016806844c2bd6647b51c
tejesh shivanna <tejeshps@gmail.com>
2018-05-11 00:28:10 -0700

    commit with failing test

--- src/test/java/HelloTeamCityTest.java
+++ src/test/java/HelloTeamCityTest.java
@@ -5,7 +5,7 @@ public class HelloTeamCityTest {
     @Test
     public void checkGreetMessage() {
         String message = "HelloTeamCity";
-        String expected = "HelloTeamCity";
+        String expected = "HelloTeamcity";
 
         HelloTeamCity helloTeamCity = new HelloTeamCity(message);
         assertEquals(helloTeamCity.getGreetMessage(), expected);
